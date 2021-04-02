a6c1c3868a406e9354e9719e557638dd51c07092
w0lfw1tz <81650693+w0lfw1tz@users.noreply.github.com>
2021-04-02 14:02:28 +0530

    Update scraper.py

--- app/scraper.py
+++ app/scraper.py
@@ -3,34 +3,29 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import unicodedata
-import flask
+import flask import Flask, jsonify
 
-res = requests.get('https://quotes.toscrape.com/tag/life')
+app = Flask(__name__)
 
-soup = BeautifulSoup(res.content, 'html.parser')
-quotes = soup.find_all("div", class_ = "quote")
 
-myQuotes = []
-entry = {}
-database = []
-
-for quote in quotes:
-  author = quote.select('small.author')[0].text
-  text = quote.select('span.text')[0].text
-  entry['author']=unicodedata.normalize('NFKD', author).encode('ascii', 'ignore').decode('utf-8')
-  entry['quote']=unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8')
-  database.append(entry)
-  entry = {}
-
-with open('database.json','w', encoding='utf8') as f:
-  json.dump(database,f)
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return str(database)
-
-app.run()
+  res = requests.get('https://quotes.toscrape.com/tag/life')
+  soup = BeautifulSoup(res.content, 'html.parser')
+  quotes = soup.find_all("div", class_ = "quote")
+  
+  myQuotes = []
+  entry = {}
+  database = []
+  
+  for quote in quotes:
+    author = quote.select('small.author')[0].text
+    text = quote.select('span.text')[0].text
+    entry['author']=unicodedata.normalize('NFKD', author).encode('ascii', 'ignore').decode('utf-8')
+    entry['quote']=unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8')
+    database.append(entry)
+    entry = {}
+  
+  return jsonify(database)
